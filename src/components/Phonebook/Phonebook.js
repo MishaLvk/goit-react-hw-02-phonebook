4289b316deec97ff0fe1e78784108b82f25b2248
Misha Levko <lvk.misha@gmail.com>
2022-11-16 14:19:29 +0200

    data_contacts_delete

--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -6,12 +6,7 @@ import ContactList from './ContactList';
 
 class Phonebook extends React.Component {
   state = {
-    contacts: [
-      { key: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { key: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { key: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { key: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
